2f90b541ee9bb26db2d3bb08cf8cd3a9a928a709
Ryan Nhân Wahlström <87569775+Bummire@users.noreply.github.com>
2024-09-24 19:51:41 +1000

    Update teacher_dashboard.py
    Additions:
    - Background Style
    - Button Style
    - Code Reformatted

--- teacher_dashboard.py
+++ teacher_dashboard.py
@@ -2,18 +2,27 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def teacher_dashboard():
-    """
-    <style>
+    # Apply background image only on the main menu
+    page_bg_img = """
+    <style> 
+        /* Meta tag to set viewport for responsive design */
+        @viewport {
+            width: device-width;
+            initial-scale: 1;
+            user-scalable=no;
+        }
+
+        [data-testid="stApp"] {
+            background-color: #ffffff;
+            opacity: 1;
+            background-image:  linear-gradient(#b7b7c1 1.4000000000000001px, transparent 1.4000000000000001px), linear-gradient(to right, #b7b7c1 1.4000000000000001px, #ffffff 1.4000000000000001px);
+            background-size: 28px 28px;
+        }
+
         h1 {
             text-align: center;
             color: black;
             font-size: 50px;
-        }
-
-        h3 {
-            text-align: center;
-            color: black;
-            font-size: 30px;
         }
 
         p {
@@ -21,13 +30,29 @@ def teacher_dashboard():
             color: black;
             font-size: 20px;
         }
+
+        /* Custom styling for the button */
+        div.stButton > button {
+            background-color: white;
+            border: 1px solid #b7b7c1;
+            color: black;
+            font-size: 10px;
+            margin-left: 8px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        div.stButton > button:hover {
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);  /* Shadow */
+        }
+
     </style>
     """
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     
     st.markdown(
         """
         <h1>Teacher's Dashboard</h1>
-        <h3>Chatbot Agent</h3>
         <p>Our fine-tuned agent is capable of generating quizzes from your inputs!</p>
         """,
         unsafe_allow_html=True
@@ -39,4 +64,4 @@ def teacher_dashboard():
     components.html(iframe_code, height=800)
 
     if st.button("Sign out"):
-        st.session_state.page = "main"
+        st.session_state.page = None
