b9a30e736285b609a263d4739a3a98071284b0dd
Ryan Nhân Wahlström <87569775+Bummire@users.noreply.github.com>
2024-09-24 19:51:01 +1000

    Update student_dashboard.py
    Additions:
    - Background Style
    - Button Style
    - Code Reformatted

--- student_dashboard.py
+++ student_dashboard.py
@@ -2,22 +2,54 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def student_dashboard():
-    """"
+    # Apply background image only on the main menu
+    page_bg_img = """
     <style> 
+        /* Meta tag to set viewport for responsive design */
+        @viewport {
+            width: device-width;
+            initial-scale: 1;
+            user-scalable=no;
+        }
+
+        [data-testid="stApp"] {
+            background-color: #ffffff;
+            opacity: 1;
+            background-image:  linear-gradient(#b7b7c1 1.4000000000000001px, transparent 1.4000000000000001px), linear-gradient(to right, #b7b7c1 1.4000000000000001px, #ffffff 1.4000000000000001px);
+            background-size: 28px 28px;
+        }
+
         h1 {
             text-align: center;
             color: black;
             font-size: 50px;
         }
-    
-        h1 {
+
+        p {
             text-align: center;
             color: black;
             font-size: 20px;
         }
+
+        /* Custom styling for the button */
+        div.stButton > button {
+            background-color: white;
+            border: 1px solid #b7b7c1;
+            color: black;
+            font-size: 10px;
+            margin-left: 8px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        div.stButton > button:hover {
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);  /* Shadow */
+        }
+
     </style>
     """
-    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     st.markdown(
         """
         <h1>Student's Dashboard</h1>
@@ -32,4 +64,4 @@ def student_dashboard():
     components.html(iframe_code, height=800)
     
     if st.button("Sign out"):
-        st.session_state.student_mode = "default"
+        st.session_state.page = None
